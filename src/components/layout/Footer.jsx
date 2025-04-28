76e7b85e7245b677d69203bd24e092f54e1d1555
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-04-28 17:03:59 +0200

    feat: create Layout component and add to Router

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <h1>This is inside the Footer component.</h1>;
+}
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h1>This inside the Header component</h1>;
+}
