638e4bf00ce1efe54161fbe46b931e4abf2313c4
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-04-29 11:45:11 +0200

    feat: add content/navlinks to header, as well as functionality for categories dropdown menu

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,51 @@
+import { NavLink } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+
 export default function Header() {
+  const [isCategoriesOpen, setIsCategoriesOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  function toggleCategories() {
+    setIsCategoriesOpen((prev) => !prev);
+  }
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsCategoriesOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <header>
-      <h1>This is the Header component</h1>
+      <nav>
+        <NavLink to="/">
+          <img
+            src="/public/assets/Logo_lightmode.png"
+            alt="Holidaze logo of a cabin"
+          />
+        </NavLink>
+        <NavLink to="Venues">All venues</NavLink>
+        <div ref={dropdownRef}>
+          <button onClick={toggleCategories}>Categories</button>
+          {isCategoriesOpen && (
+            <div>
+              <NavLink to="Categories">Forest</NavLink>
+              <NavLink to="Categories">Sea</NavLink>
+              <NavLink to="Categories">Mountain</NavLink>
+            </div>
+          )}
+        </div>
+        <NavLink to="About">About us</NavLink>
+        <NavLink to="Register">Register</NavLink>
+        <NavLink to="Login">Log in</NavLink>
+      </nav>
     </header>
   );
 }
