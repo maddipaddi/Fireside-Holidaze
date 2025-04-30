5e65c2ad65aed7e2beb7d5d90156a31f38d38671
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-04-30 09:42:50 +0200

    feat: implement light and dark mode in header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -30,48 +30,60 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="flex items-center justify-between p-4">
+    <header className="flex items-center justify-between p-4 bg-background dark:bg-copy">
       <NavLink to="/">
         <img
           src="/assets/Logo_lightmode.png"
           alt="Holidaze logo of a cabin"
-          className="h-16 md:h-24 lg:h-32"
+          className="block dark:hidden h-16 md:h-24 lg:h-32"
+        />
+        <img
+          src="/assets/Logo_darkmode.png"
+          alt="Holidaze logo of a cabin"
+          className="hidden dark:block h-16 md:h-24 lg:h-32"
         />
       </NavLink>
       <button
-        className="md:hidden cursor-pointer"
+        className="md:hidden cursor-pointer text-copy dark:text-background"
         onClick={toggleMenu}
         aria-label="Toggle menu"
       >
         {menuOpen ? <X size={32} /> : <Menu size={32} />}
       </button>
       <nav
-        className={`${menuOpen ? "flex" : "hidden"} absolute top-20 left-0 w-full flex-col gap-4 p-4 bg-green-900 text-white md:bg-white md:text-black shadow-md md:static md:flex md:flex-row md:flex-grow md:justify-evenly md:w-auto md:gap-8 md:shadow-none md:text-l lg:text-xl`}
+        className={`${menuOpen ? "flex" : "hidden"} fixed top-20 left-0 w-full z-50 items-center flex-col gap-4 p-8 t shadow-md md:static md:flex md:flex-row md:flex-grow md:justify-evenly md:w-auto md:gap-8 md:shadow-none md:text-l lg:text-xl bg-background dark:bg-copy text-copy dark:text-background`}
       >
-        <NavLink to="Venues" className="mx-6">
+        <NavLink to="Venues" className="font-heading">
           All Venues
         </NavLink>
         <div ref={dropdownRef}>
-          <button onClick={toggleCategories} className="cursor-pointer mx-6">
+          <button
+            onClick={toggleCategories}
+            className="cursor-pointer font-heading"
+          >
             Categories
           </button>
           {isCategoriesOpen && (
-            <div>
-              <NavLink to="Categories" className="mx-6">
+            <div className="flex gap-3">
+              <NavLink to="Categories" className="font-body">
                 Forest
               </NavLink>
-              <NavLink to="Categories">Sea</NavLink>
-              <NavLink to="Categories">Mountain</NavLink>
+              <NavLink to="Categories" className="font-body">
+                Sea
+              </NavLink>
+              <NavLink to="Categories" className="font-body">
+                Mountain
+              </NavLink>
             </div>
           )}
         </div>
-        <NavLink to="About" className="mx-6">
+        <NavLink to="About" className="font-heading">
           About us
         </NavLink>
-        <NavLink to="Register" className="mx-6">
+        <NavLink to="Register" className="font-heading">
           Register
         </NavLink>
-        <NavLink to="Login" className="mx-6">
+        <NavLink to="Login" className="font-heading">
           Log in
         </NavLink>
       </nav>
