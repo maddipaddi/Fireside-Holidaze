bf6852409e4455a2b949752d3b08e8da9bc705fc
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-05-06 15:55:35 +0200

    feat: add navigation with button function to single venue

--- src/components/VenueCards.jsx
+++ src/components/VenueCards.jsx
@@ -1,29 +1,44 @@
+import { useNavigate } from "react-router-dom";
+
 function VenueCards({ venues }) {
-    if (!venues || venues.length === 0) {
-      return <p>No venues found</p>;
-    }
-  
-    return (
-      <section>
-        <div>
-          {venues.map((venue) => (
-            <div key={venue.id}>
-              <img 
-                src={venue.media?.[0]?.url || "https://placehold.co/150x150?text=No+Image"}
-                alt={venue.media?.[0]?.alt || venue.name} 
-              />
-              <div>
-                <h2>{venue.name}</h2>
-                <p>${venue.price}</p>
-              </div>
-              <div>
-                <p>*{venue.rating}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-    );
+  const navigate = useNavigate();
+
+  if (!venues || venues.length === 0) {
+    return <p>No venues found</p>;
   }
-  
-  export default VenueCards;
+
+  return (
+    <section>
+      <div>
+        {venues.map((venue) => (
+          <div key={venue.id}>
+            <img
+              src={
+                venue.media?.[0]?.url ||
+                "https://placehold.co/150x150?text=No+Image"
+              }
+              alt={venue.media?.[0]?.alt || venue.name}
+            />
+            <div>
+              <h2>{venue.name}</h2>
+              <p>${venue.price}</p>
+            </div>
+            <div>
+              <p>*{venue.rating}</p>
+            </div>
+            <div>
+              <button
+                className="text-white cursor-pointer"
+                onClick={() => navigate(`/venue/${venue.id}`)}
+              >
+                View Venue
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default VenueCards;
