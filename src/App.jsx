9ae74525904b3ed6b1724065007493553ebb6424
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-05-06 15:53:16 +0200

    feat: add route to single venue

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,20 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Terms from "./pages/Terms";
 import Privacy from "./pages/Privacy";
-
+import SingleVenue from "./pages/SingleVenue";
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/Venues" element={<Venues/>} />
+          <Route path="/Venues" element={<Venues />} />
           <Route path="/Categories" element={<Categories />} />
           <Route path="/About" element={<About />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
+          <Route path="/venue/:id" element={<SingleVenue />} />
           <Route path="/Terms" element={<Terms />} />
           <Route path="/Privacy" element={<Privacy />} />
         </Route>
