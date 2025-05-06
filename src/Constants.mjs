f880d228d18611b51f56df2808703b709d81e275
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-05-06 15:53:48 +0200

    feat: single venue const

--- src/Constants.mjs
+++ src/Constants.mjs
@@ -1,4 +1,5 @@
+export const BASE_URL = `https://v2.api.noroff.dev/holidaze`;
 
-export const BASE_URL = `https://v2.api.noroff.dev/holidaze`
+export const ALL_USERS = `${BASE_URL}/venues`;
 
-export const ALL_USERS = `${BASE_URL}/venues`
+export const SINGLE_VENUE = `${ALL_USERS}`;
