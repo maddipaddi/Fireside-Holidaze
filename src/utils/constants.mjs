8cc6ea70358bd2c95352d4c40a996fdce18f8e9b
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-05-07 10:03:17 +0200

    feat: display venue manager's venues on their profile page

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -3,3 +3,5 @@ export const BASE_URL = `https://v2.api.noroff.dev/holidaze`;
 export const VENUES = `${BASE_URL}/venues`;
 
 export const SINGLE_VENUE = `${VENUES}`;
+
+export const PROFILE_VENUES = `${BASE_URL}/profiles`;
