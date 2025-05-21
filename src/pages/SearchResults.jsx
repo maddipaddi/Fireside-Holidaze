3719c3a7e41dc97c279fbc355b03cb8312c682fa
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-05-21 13:14:05 +0200

    feat: make search results look more uniform

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -142,21 +142,26 @@ const SearchResults = () => {
               key={venue.id}
               venue={venue}
               renderFooter={(v) => (
-                <>
+                <div className="min-h-48">
                   <h3 className="text-lg font-bold mb-1">{v.name}</h3>
-                  <p className="text-sm">{v.description}</p>
-                  {!v.isAvailable && (
-                    <p className="bg-darkbackground text-center rounded py-2 mt-2 italic font-medium">
-                      Not available for selected dates
-                    </p>
-                  )}
+                  <p className="text-sm">
+                    {v.description.length > 150
+                      ? `${v.description.slice(0, 150)}…`
+                      : v.description}
+                  </p>
+
                   <button
                     onClick={() => navigate(`/venue/${v.id}`)}
                     className="text-sm underline mt-2 hover:text-accent"
                   >
                     Read more
                   </button>
-                </>
+                  {!v.isAvailable && (
+                    <p className="bg-darkbackground text-center rounded py-2 mt-2 italic font-medium">
+                      Not available for selected dates
+                    </p>
+                  )}
+                </div>
               )}
             />
           ))}
