37754a3269503bbf41d3c02ff09583b7a7bc9dbb
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-05-22 12:46:11 +0200

    feat: associate label and input on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -118,15 +118,20 @@ export default function Profile() {
             Hi, {user.name}!
           </h1>
           <div className="mb-6">
-            <label className="block text-sm font-semibold font-body text-white mb-1">
+            <label
+              htmlFor="profile-img-url"
+              className="block text-sm font-semibold font-body text-white mb-1"
+            >
               Change profile picture
             </label>
             <input
+              name="profile-img-url"
+              id="profile-img-url"
               type="text"
               value={newAvatarUrl}
               onChange={(e) => setNewAvatarUrl(e.target.value)}
               className="font-body w-full px-3 py-2 bg-white text-black border rounded focus:outline-none focus:ring focus:border-copy"
-              placeholder="Paste image URL here"
+              placeholder="https://url.com/image.jpg"
             />
           </div>
 
