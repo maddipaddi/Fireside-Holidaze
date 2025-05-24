f734a959b586d16c42394f982e9f6cf47128b3d0
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-05-24 16:08:43 +0200

    refactor: split giga component AddVenue.jsx into smaller functions

---
+++ src/components/AddVenue/BasicInfoSection.jsx
@@ -0,0 +1,48 @@
+export default function BasicInfoSection({ data, onChange }) {
+  return (
+    <section className="md:col-span-1 xl:col-span-2">
+      <h3 className="text-lg font-bold font-body text-white mb-2 text-center">
+        Basic info
+      </h3>
+      <div className="rounded-xl bg-offwhite dark:bg-background p-4 mb-4">
+        <label
+          htmlFor="name"
+          className="block text-sm font-semibold font-body text-copy dark:text-copy mb-1"
+        >
+          Venue name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          minLength={2}
+          maxLength={30}
+          required
+          value={data.name}
+          onChange={onChange}
+          placeholder="Cozy cabin"
+          className="font-body w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-copy dark:bg-white dark:text-copy bg-white"
+        />
+      </div>
+      <div className="rounded-xl bg-offwhite dark:bg-background p-4 mb-4">
+        <label
+          htmlFor="description"
+          className="block text-sm font-semibold font-body text-copy dark:text-copy mb-1"
+        >
+          Description
+        </label>
+        <textarea
+          name="description"
+          id="description"
+          rows={5}
+          maxLength={500}
+          required
+          value={data.description}
+          onChange={onChange}
+          placeholder="This amazing cabin sits on the top of a cliff, overlooking the sea..."
+          className="font-body w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-copy dark:bg-white dark:text-copy"
+        />
+      </div>
+    </section>
+  );
+}
